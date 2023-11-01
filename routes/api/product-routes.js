8d2fa470a019da3781802976f301835d5011c82d
gfuselier <gisellefuselier@gmail.com>
2023-11-01 18:31:26 -0500

    included tag alias

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // includes its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category}, {model: Tag}]
+      include: [{ model: Category}, {model: Tag, as: 'product_tags'}]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // includes its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category}, {model: Tag}] 
+      include: [{ model: Category}, {model: Tag, as: 'product_tags'}] 
     });
 
     if (!productData) {
